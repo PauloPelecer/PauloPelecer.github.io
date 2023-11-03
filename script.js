5afd006b8e91b85bd873f9bca2985e5ca26dae97
SenhorLoock <71995271+PauloPelecer@users.noreply.github.com>
2023-11-03 11:36:15 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function startAnimation(){const t=document.querySelectorAll("img.img");t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="700px"}),t.addEventListener("mouseleave",()=>{t.style.width="350px"})})}
-function startAnimation(){const t=document.querySelectorAll("img.img_mobile");t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="350px"}),t.addEventListener("mouseleave",()=>{t.style.width="150px"})})}
-function startInputAnimation(){
-  const t=document.querySelectorAll("input");
-t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="300px"}),t.addEventListener("mouseleave",()=>{t.style.width="250px"})})}
-window.addEventListener("load",startAnimation),window.addEventListener("load",startInputAnimation);
+function startAnimation() {
+  const images = document.querySelectorAll("img.img");
+  images.forEach(image => {
+    image.style.transition = "width 0.5s";
+    image.addEventListener("mouseenter", () => {
+      image.style.width = "700px";
+    });
+    image.addEventListener("mouseleave", () => {
+      image.style.width = "350px";
+    });
+  });
+}
+
+function startMobileAnimation() {
+  const mobileImages = document.querySelectorAll("img.img_mobile");
+  mobileImages.forEach(image => {
+    image.style.transition = "width 0.5s";
+    image.addEventListener("mouseenter", () => {
+      image.style.width = "350px";
+    });
+    image.addEventListener("mouseleave", () => {
+      image.style.width = "150px";
+    });
+  });
+}
+
+function startInputAnimation() {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach(input => {
+    input.style.transition = "width 0.5s";
+    input.addEventListener("mouseenter", () => {
+      input.style.width = "300px";
+    });
+    input.addEventListener("mouseleave", () => {
+      input.style.width = "250px";
+    });
+  }
+}
+
+window.addEventListener("load", startAnimation);
+window.addEventListener("load", startMobileAnimation);
+window.addEventListener("load", startInputAnimation);
