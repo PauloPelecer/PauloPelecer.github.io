3a420504647f9d730cf9becc1dd79c177736b491
SenhorLoock <71995271+PauloPelecer@users.noreply.github.com>
2023-11-03 11:32:41 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function startAnimation(){const t=document.querySelectorAll("img.img");t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="700px"}),t.addEventListener("mouseleave",()=>{t.style.width="350px"})})}
-function startAnimation(){const t=document.querySelectorAll("img.img_mobile");t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="350px"}),t.addEventListener("mouseleave",()=>{t.style.width="150px"})})}
+//function startAnimation(){const t=document.querySelectorAll("img.img_mobile");t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="350px"}),t.addEventListener("mouseleave",()=>{t.style.width="150px"})})}
 function startInputAnimation(){
   const t=document.querySelectorAll("input");
 t.forEach(t=>{t.style.transition="width 0.5s",t.addEventListener("mouseenter",()=>{t.style.width="300px"}),t.addEventListener("mouseleave",()=>{t.style.width="250px"})})}
